181fb4a36b3a629869eb71e26cfbc110f322ae8f
evilenx <evilenx@proton.me>
2024-06-17 18:52:23 -0500

    add changes

--- src/crystalfetch.cr
+++ src/crystalfetch.cr
@@ -1,29 +1,26 @@
 require "system"
 
 class CrystalFetch
-   def run()
-      get_os()
-      get_hostname()
-      get_cpu_count()
-   end
-   def get_os()
-      system("uname -a")
-   end
-   def get_hostname()
-      name = System.hostname
-      puts "#{name}"
-   end
-   def get_cpu_count()
-      cpu_num = System.cpu_count
-      puts "#{cpu_num}"
-   end
+  def run
+    get_os()
+    get_hostname()
+    get_cpu_count()
+  end
+
+  def get_os
+    system("uname -a")
+  end
+
+  def get_hostname
+    name = System.hostname
+    puts "Hostname: #{name}"
+  end
+
+  def get_cpu_count
+    cpu_num = System.cpu_count
+    puts "CPU: #{cpu_num}"
+  end
 end
 
-struct Information
-   property hostname;
-   property cpu_count;
-   property os_name;
-end
-
-fetch = CrystalFetch.new()
-fetch.run()
+fetch = CrystalFetch.new
+fetch.run
